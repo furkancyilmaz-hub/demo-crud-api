d5a60ba79b9a5ef8763c9365f60ff588433d86b4
root <root@DESKTOP-E2BVL0R.localdomain>
2026-08-01 00:36:04 +0300

    Initial commit

---
+++ src/main/java/com/furkan/democrudapi/DemoCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.furkan.democrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCrudApiApplication.class, args);
+    }
+
+}
