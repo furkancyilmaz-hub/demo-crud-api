a70f4cf18d1d651cef99730c53d1272b40363d8a
furkancyilmaz-hub <cevfurkanyilmaz@gmail.com>
2026-08-02 00:56:39 +0300

    Move internal controllers to controller package
    Controller sınıflarını teknik katman paketleme kuralına uymak için
    com.furkan.democrudapi.internal'den com.furkan.democrudapi.controller'a taşır.

--- src/main/java/com/furkan/democrudapi/controller/InternalToggleController.java
+++ src/main/java/com/furkan/democrudapi/controller/InternalToggleController.java
@@ -1,4 +1,4 @@
-package com.furkan.democrudapi.internal;
+package com.furkan.democrudapi.controller;
 
 import com.furkan.democrudapi.config.BugProperties;
 import com.furkan.democrudapi.dto.BugToggleResponse;
